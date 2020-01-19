26522d00c00b5825f4e4a8921dc3667568aee358
taslimy <taslimer@gmail.com>
2020-01-19 17:56:17 -0500

    fixing link on frontend and deployment on zeit

--- src/component/login.js
+++ src/component/login.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import './login.styles.scss'
+import "./login.styles.scss";
 
 const Login = () => {
   return (
     <div className="login-wrapper">
-      <a href="http://localhost:8888/login">login to spotify to begin</a>
-      {/* <a href="https://your-spotify.taslim.now.sh/login">
+      {/* <a href="http://localhost:8888/login">login to spotify to begin</a> */}
+      <a href="https://your-spotify.taslim.now.sh/login">
         login to spotify to begin
-      </a> */}
+      </a>
     </div>
   );
 };
